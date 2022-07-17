3f4bf942fd6cc972a7a6658a36172bdedafdb541
Paul Taylor <paul@whammydiver.com>
2022-07-17 15:21:46 -0400

    add counter color change and tweetbox formatting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,15 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+$(document).ready(function() {
+  const tweetContainer = document.getElementById("tweetContainer");
+
+  tweetContainer.addEventListener("mouseover", () => {
+    tweetContainer.classList.add("shadow");
+  });
+
+  tweetContainer.addEventListener("mouseout", () => {
+    tweetContainer.classList.remove("shadow"); 
+  })
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,18 @@
 let charCount = 140;
-console.log(charCount);
 
 $(document).ready(function() {
   let counter = document.getElementById("counter");
-  console.log(counter);
   const textarea = document.getElementById("tweet-text-box");
   
   const charCounter = function() {
     textarea.addEventListener("input", (event) => {
-      console.log(event);
       charCount = 140 - (textarea.value.length);
-      // if (event.inputType !== "deleteContentBackward") {
-      //   charCount -= 1;
-      // } else {
-      //   charCount += 1;
-      // }
-      console.log(event.inputType, charCount);
-      return charCount;
+      counter.innerHTML = charCount;
+      if (charCount < 0) {
+        counter.classList.add("redCounter");
+      } else {
+        counter.classList.remove("redCounter");
+      }
     });
   };
   charCounter();
