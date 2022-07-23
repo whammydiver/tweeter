497603ed60fc8c2933296e6081fea1417f84a671
Paul Taylor <paul@whammydiver.com>
2022-07-23 16:46:40 -0400

    back to top button working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,47 +1,30 @@
 $(document).ready(function() {
-
-  $(".errorMessage").hide();
-
-  $(".fa-angles-down").on("click", function() {
-    if ($(".new-tweet").is(":hidden")) {
-      $(".new-tweet").slideDown("fast");
-    } else {
-      $(".new-tweet").slideUp("fast");
-    }
-  });
-
+  
   loadTweets();
   
-  $("#newTweet").on("submit", function(event) {
-    event.preventDefault();
-    const data = ($("textarea").serialize());
-    if ($("textarea").val().length === 0) {
-      alerts("You cannot submit a blank tweet. Please enter some text before tweeting.");
-    } else if ($("textarea").val().length > 140) {
-      alerts("Tweet exceeds the 140 character limit, as evidenced by the character counter. Try again, more succinctly.");
-    } else {
-      $.post("/tweets", data)
-      .then(() => {
-        loadTweets();
-      })
-      location.reload();
-    };
-  })
+  tweetSlider();
+  
+  newTweet();
+  
+  $(".errorMessage").hide();
+
+  $("#top").hide();
+
+  returnButton();
+
 });
 
 const loadTweets = () => {
   $.get("/tweets")
     .then(data => {
       renderTweets(data);
-    })
-}
+    });
+};
 
 const alerts = function(str) {
   $(".errorMessage").html(str);
   $(".errorMessage").show();
-}
-
-
+};
 
 const renderTweets = function(tweets) {
   const container = $(".tweetsContainer")
@@ -56,8 +39,6 @@ const safeTextMaker = function (str) {
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
-
-
 
 const createTweetElement = function(tweetData) {
 
@@ -86,5 +67,44 @@ const createTweetElement = function(tweetData) {
 
 };
   
+const tweetSlider = function() {
+  $(".fa-angles-down").on("click", function() {
+    if ($(".new-tweet").is(":hidden")) {
+      $(".new-tweet").slideDown("fast");
+    } else {
+      $(".new-tweet").slideUp("fast");
+    }
+  });
+};
 
+const newTweet = function() {
+  $("#newTweet").on("submit", function(event) {
+    event.preventDefault();
+    const data = ($("textarea").serialize());
+    if ($("textarea").val().length === 0) {
+      alerts("You cannot submit a blank tweet. Please enter some text before tweeting.");
+    } else if ($("textarea").val().length > 140) {
+      alerts("Tweet exceeds the 140 character limit, as evidenced by the character counter. Try again, more succinctly.");
+    } else {
+      $.post("/tweets", data)
+      .then(() => {
+        loadTweets();
+      })
+      location.reload();
+    };
+  })
+}
+
+const returnButton = function() {
+  $(window).scroll(() => {
+    if (window.scrollY > 0) {
+      $("#top").show();
+    } else {
+      $("#top").hide();
+    }
+  });
+  $("#top").on("click", () => {
+    $("html, body").animate({scrollTop: 0}, "fast");
+  })
+};
 
